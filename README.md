b38bbde1d976130e8477c31735b748b102f6ec85
Sae Pasomba <saepasomba12@gmail.com>
2022-03-21 22:25:33 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## API for test
+
+This API will be used for my assignment about load, endurance, and stress testing.
---
+++ main.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+import shutil
+from fastapi import Body, FastAPI, UploadFile
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Mahasiswa(BaseModel):
+    nama: str
+    npm: str
+    alamat: str
+
+db = {}
+
+@app.get("/mahasiswa/{npm}")
+def get_mahasiswa(npm: str):
+    return db[npm]
+
+@app.post("/mahasiswa")
+def post_mahasiswa(mahasiswa: Mahasiswa):
+    npm = mahasiswa.npm
+    db[npm] = mahasiswa
+    return mahasiswa
+
+@app.put("/mahasiswa/{npm}")
+def edit_mahasiswa(npm: str, nama: str = Body(...), alamat: str = Body(...)):
+    db[npm].nama = nama
+    db[npm].alamat = alamat
+    return db[npm]
+
+@app.delete("/mahasiswa/{npm}")
+def delete_mahasiswa(npm: str):
+    db.pop(npm)
+    return {
+        "npm" : npm,
+        "status" : "Deleted"
+    }
+
+@app.post("/upload")
+def upload_file(file: UploadFile):
+    file_location = f"files/{file.filename}"
+    with open(file_location, "wb+") as file_object:
+        shutil.copyfileobj(file.file, file_object)
+    return {
+        "filename" : file.filename,
+        "status" : "Success"
+        }
+
